06cb500444b3b4f25014435177c65167bbc821d9
Mahmoud Oluwatobi <mahmoud4u12@gmail.com>
2022-11-11 15:15:13 +0100

    chore/Add necessary boilerplates and folder structure

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# MetaBnB
+A Landing page concept for NFT virtual Homes you can own in the Metaverse
---
+++ src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="App">Hello wud</div>;
+}
+
+export default App;
