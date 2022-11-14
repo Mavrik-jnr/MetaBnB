95a5e5f29cd40c49d4f6a6303f073b360de97c8a
Mahmoud Oluwatobi <mahmoud4u12@gmail.com>
2022-11-14 17:35:34 +0100

    style/Add hover effect to secondary button

--- src/components/Button.js
+++ src/components/Button.js
@@ -18,6 +18,7 @@ export const PrimaryButton = styled.button`
   background-color: var(--primary);
   color: var(--white);
   padding: 12px 20px;
+  cursor: pointer;
   border-radius: 10px;
   outline: none;
   border: none;
@@ -25,4 +26,15 @@ export const PrimaryButton = styled.button`
 const SecondaryButton = styled(PrimaryButton)`
   background-color: var(--white);
   color: var(--primary);
+  cursor: pointer;
+
+  &:hover {
+    background-image: linear-gradient(
+      91.47deg,
+      #dc3e88 18.25%,
+      #b840ae 43.34%,
+      #843fe8 78.18%
+    );
+    color: white;
+  }
 `;
